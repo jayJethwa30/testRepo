4dfc9104ae346370b04390167dea6f3a4bc5541d
Jay Jethwa <jethwa.jay30@gmail.com>
2020-07-26 17:51:37 +0530

    Added Hello world from Master

--- Sample2.java
+++ Sample2.java
@@ -4,5 +4,6 @@ public class Sample2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello World 2");
+        System.out.println("Hello World from Master");
     }
 }
