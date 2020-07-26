6df4ea23446d75d59e7ddf599f65e8fbfddf67ec
Jay Jethwa <jethwa.jay30@gmail.com>
2020-07-26 17:25:29 +0530

    Added Hello World 2

--- Sample2.java
+++ Sample2.java
@@ -3,5 +3,6 @@ import java.lang.*;
 public class Sample2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello World 2");
     }
 }
