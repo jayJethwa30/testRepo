bed748f62cd218728dab80a1edc789048daf7862
Jay Jethwa <jethwa.jay30@gmail.com>
2020-07-26 18:19:24 +0530

    Corrected file commited

--- Sample2.java
+++ Sample2.java
@@ -4,7 +4,6 @@ public class Sample2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello World 2");
-        System.out.println("Hello World from Master");
-        System.out.println("Hello world from Slave1");
+        System.out.println("Hello World 3");
     }
 }
